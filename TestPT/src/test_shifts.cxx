cd4ef6d8aa51fb2ce40e488b26d7b44929c7c1df
Allen Wen <acw6923@g.rit.edu>
2023-06-20 10:15:13 -0400

    added y axis rotation test case

--- TestPT/src/test_shifts.cxx
+++ TestPT/src/test_shifts.cxx
@@ -32,4 +32,24 @@ extern "C" void TestPT_init_shift_circle(CCTK_ARGUMENTS) {
                                     });
 }
 
+extern "C" void TestPT_init_shift_3D(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTS_TestPT_init_shift_3D;
+  DECLARE_CCTK_PARAMETERS;
+
+  const array<int, dim> indextype = {0, 0, 0};
+  const GF3D2layout layout(cctkGH, indextype);
+
+  const GF3D2<CCTK_REAL> betax_(layout, betax);
+  const GF3D2<CCTK_REAL> betay_(layout, betay);
+  const GF3D2<CCTK_REAL> betaz_(layout, betaz);
+
+  const GridDescBaseDevice grid(cctkGH);
+  grid.loop_all_device<0, 0, 0>(grid.nghostzones,
+                                [=] CCTK_DEVICE(const PointDesc &p)
+                                    CCTK_ATTRIBUTE_ALWAYS_INLINE {
+                                      betax_(p.I) = p.z;
+                                      betay_(p.I) = 0;
+                                      betaz_(p.I) = -p.x;
+                                    });
+}
 } //namespace TestPT
